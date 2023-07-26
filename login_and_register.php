b6d2296a59a5012fa7c14b24502fee8d333e490b
User_20200273 <allen.pagnanawon@foundationu.com>
2023-07-26 14:20:40 +0800

    before ajax

--- login_and_register.php
+++ login_and_register.php
@@ -6,7 +6,15 @@ error_reporting(E_ALL);
 <!DOCTYPE HTML>
 <html>
 <head>
-<link rel="stylesheet" type="text/css" href="css/style.css"/>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>FUCS</title>
+
+    <!-- jQuery --> 
+    <script src="lib/jquery.min.js"></script>
+
+    <!-- Custom -->
+    <link rel="stylesheet" type="text/css" href="css/style.css"/>
 </head>
 <body>
 <?php
